64af0a80b23fb8a1f1bf41b42bc6064ffd0b3b1d
pardeep kaur <137564227+Pardeepkaur32@users.noreply.github.com>
2024-10-23 11:47:42 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
-#install docker                                                                                                                                             # Add Docker's official GPG key:                                                                                                                            sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# Add the repository to Apt sources:                                                                                                                        echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#install docker
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo usermod -aG docker ubuntu                                                                                                                              newgrp docker                                                                                                                                               ~              
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
